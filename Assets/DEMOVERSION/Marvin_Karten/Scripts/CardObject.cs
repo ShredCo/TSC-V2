685b0a7f169bd4fc79ebdaa5d51f099637b8e50d
Fabian <faebu.og@gmail.com>
2022-03-27 03:13:56 +0200

    UI

--- Assets/DEMOVERSION/Marvin_Karten/Scripts/CardObject.cs
+++ Assets/DEMOVERSION/Marvin_Karten/Scripts/CardObject.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class CardObject : MonoBehaviour
 {
-    public Cards cardObject;
+    //public Cards cardObject;
 
     private void Start()
     {
-        GameObject visuals = Instantiate(cardObject.cardModel);
-        visuals.transform.SetParent(this.transform);
-        visuals.transform.localPosition = Vector3.zero;
+        //GameObject visuals = Instantiate(cardObject.cardModel);
+        //visuals.transform.SetParent(this.transform);
+        //visuals.transform.localPosition = Vector3.zero;
     }
 
 }
