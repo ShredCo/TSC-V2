e01d63ca6f24847b27ed37e5c7093845e4a027a7
FaebuOG <soly.fabian.g@gmail.com>
2022-04-03 22:51:30 +0200

    worked on the shooting of the AI.
    Shooting is working, but the timers and enum states not working yet

---
+++ Assets/_TSC/_Scripts/AI/AIShootingTriggerBoxes.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+
+public enum ShootingState
+{
+    Default,
+    Loaded,
+    Shooted
+}
+public class AIShootingTriggerBoxes : MonoBehaviour
+{
+    public CrewPole2AI crewPole2AI;
+
+    private float currentCount = 0f;
+    private float startCountDown = 2f;
+
+    public ShootingState shootingState;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            currentCount = startCountDown;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        shootingState = ShootingState.Default;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            currentCount -= 1f * Time.deltaTime;
+            Debug.Log(currentCount);
+            if (currentCount <= 0)
+            {
+                // loads the shot
+                var step = 300 * Time.deltaTime;
+                Quaternion loadShot = Quaternion.Euler(0,0,-45);
+                Quaternion loadedShot = Quaternion.RotateTowards(transform.rotation, loadShot, step);
+                crewPole2AI.rb.MoveRotation(loadedShot);
+                shootingState = ShootingState.Loaded;
+            }
+
+            if (shootingState == ShootingState.Loaded)
+            {
+                currentCount = startCountDown;
+
+                if (currentCount <= 1)
+                {
+                    // Shoots the Ball
+                    var step = 2000 * Time.deltaTime; 
+                    Quaternion shootShot = Quaternion.Euler(0,0,45);
+                    Quaternion shootedShot = Quaternion.RotateTowards(transform.rotation, shootShot, step);
+                    crewPole2AI.rb.MoveRotation(shootedShot);
+                    shootingState = ShootingState.Shooted;
+                }
+            }
+        }
+    }
+}
--- Assets/_TSC/_Scripts/AI/CrewPole2AI.cs
+++ Assets/_TSC/_Scripts/AI/CrewPole2AI.cs
@@ -9,7 +9,7 @@ public class CrewPole2AI : MonoBehaviour
     public float difficulty = 0.01f;
     public Transform ball;
 
-    private Rigidbody rb;
+    public Rigidbody rb;
 
     private float poleMovement;
     private Transform newPolePosition;
