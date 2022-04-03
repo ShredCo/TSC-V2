14fac4448634063cffc32242c922cbd449130180
Marvin Moreno <marmor0416@gmail.com>
2022-04-03 13:37:28 +0200

    Added LineUpController Script

---
+++ Assets/_TSC/_Scripts/Inventory Line Up/LineUpController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineUpController : MonoBehaviour
+{
+    // Arrays to save Player Line Up
+    public static DefaultCardObject[] PlayerDefaultCardLineUP = new DefaultCardObject[4];
+    public static SpecialCardObject[] PlayerSpecialCardLineUP = new SpecialCardObject[4];
+
+    // Arrays to save AI Line Up
+    public static DefaultCardObject[] AIDefaultCardLineUP = new DefaultCardObject[4];
+    public static SpecialCardObject[] AISpecialCardLineUP = new SpecialCardObject[4];
+}
