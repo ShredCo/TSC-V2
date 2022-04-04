040b64a4663b09c8bb444d63fed1dc1add0e0c4d
FaebuOG <soly.fabian.g@gmail.com>
2022-04-04 11:22:13 +0200

    AI. Shooted the first goal
    WOOOOOHOOOOOOO

---
+++ Assets/_TSC/_Scripts/AI/AITriggerbox1.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AITriggerbox1 : MonoBehaviour
+{
+    public CrewPole1AI crewPole1AI;
+
+    public ShootingState shootingState;
+
+    public IEnumerator LoadShot()
+    {
+        yield return new WaitForSeconds(2);
+        // loads the shot
+        var step = 300 * Time.deltaTime;
+        Quaternion loadShot = Quaternion.Euler(0,0,-45);
+        Quaternion loadedShot = Quaternion.RotateTowards(transform.rotation, loadShot, step);
+        crewPole1AI.rb.MoveRotation(loadedShot);
+        shootingState = ShootingState.Shooting;
+    }
+    
+    public IEnumerator ShootShot()
+    {
+        yield return new WaitForSeconds(2);
+        // Shoots the Ball
+        var step = 500 * Time.deltaTime; 
+        Quaternion shootShot = Quaternion.Euler(0,0,45);
+        Quaternion shootedShot = Quaternion.RotateTowards(transform.rotation, shootShot, step);
+        crewPole1AI.rb.MoveRotation(shootedShot);
+        shootingState = ShootingState.Default;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            shootingState = ShootingState.Loading;
+        }
+        
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        shootingState = ShootingState.Default;
+    }
+
+    void FixedUpdate()
+    {
+        switch (shootingState)
+        {
+            case ShootingState.Default:
+                break;
+            case ShootingState.Loading:
+                StartCoroutine(LoadShot());
+                break;
+            case ShootingState.Shooting:
+                StartCoroutine(ShootShot());
+                break;
+        }
+    }
+}
---
+++ Assets/_TSC/_Scripts/AI/AITriggerboxMain.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+
+public enum ShootingState
+{
+    Default,
+    Loading,
+    Shooting
+}
+public class AITriggerboxMain : MonoBehaviour
+{
+    public MainPoleAI mainPoleAI;
+
+    public ShootingState shootingState;
+
+    public IEnumerator LoadShot()
+    {
+        yield return new WaitForSeconds(2);
+        // loads the shot
+        var step = 300 * Time.deltaTime;
+        Quaternion loadShot = Quaternion.Euler(0,0,-45);
+        Quaternion loadedShot = Quaternion.RotateTowards(transform.rotation, loadShot, step);
+        mainPoleAI.rb.MoveRotation(loadedShot);
+        shootingState = ShootingState.Shooting;
+    }
+    
+    public IEnumerator ShootShot()
+    {
+        yield return new WaitForSeconds(2);
+        // Shoots the Ball
+        var step = 2000 * Time.deltaTime; 
+        Quaternion shootShot = Quaternion.Euler(0,0,45);
+        Quaternion shootedShot = Quaternion.RotateTowards(transform.rotation, shootShot, step);
+        mainPoleAI.rb.MoveRotation(shootedShot);
+        shootingState = ShootingState.Default;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            shootingState = ShootingState.Loading;
+        }
+        
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        shootingState = ShootingState.Default;
+    }
+
+    void FixedUpdate()
+    {
+        switch (shootingState)
+        {
+            case ShootingState.Default:
+                break;
+            case ShootingState.Loading:
+                StartCoroutine(LoadShot());
+                break;
+            case ShootingState.Shooting:
+                StartCoroutine(ShootShot());
+                break;
+        }
+    }
+}
