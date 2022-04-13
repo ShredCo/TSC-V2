d6c799af8fe1a7b086ded350adcb99e9b9f2c999
FaebuOG <soly.fabian.g@gmail.com>
2022-04-13 17:42:00 +0200

    ai defense fix

--- Assets/_TSC/_Scripts/AI/CrewPole2AI.cs
+++ Assets/_TSC/_Scripts/AI/CrewPole2AI.cs
@@ -5,34 +5,40 @@ using UnityEngine;
 public class CrewPole2AI : MonoBehaviour
 {
     [SerializeField] public Sense sense;
-
-    
-    [SerializeField] private float difficulty = 0.01f;
-    
+ 
     public Transform ball;
     public Rigidbody rb;
 
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float smoothSpeed = 5f;
+
     private float poleMovement;
     private Transform newPolePosition;
+
+    [SerializeField] private Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
 
     private void Start()
     {
         newPolePosition = transform;
         rb = GetComponent<Rigidbody>();
-
-        var myValue = Mathf.Lerp(0, 10, 0.5f);
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (sense.closestPlayer.name == "pos4" || sense.closestPlayer.name == "pos5" || sense.closestPlayer.name == "pos6" || sense.closestPlayer.name == "pos7" || sense.closestPlayer.name == "pos8")
         {
             // Calculate the difference between the ball and the closest enemy player
             poleMovement = sense.closestPlayer.transform.position.z - ball.transform.position.z;
             // Calculate new position of pole
-            newPolePosition.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - poleMovement);
+            //newPolePosition.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - poleMovement);
             // Set new position with Lerp
-            rb.transform.position = Vector3.Lerp(transform.position, newPolePosition.position, difficulty * Time.deltaTime);
+            //rb.transform.position = Vector3.Lerp(transform.position, newPolePosition.position, smoothSpeed * Time.deltaTime);
+
+
+            Vector3 desiredPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z - poleMovement);
+            rb.transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
         }
 
         rb.transform.position = new Vector3(Mathf.Clamp(transform.position.x, -0.1f, -0.1f), Mathf.Clamp(transform.position.y, 0.1116f, 0.1116f), Mathf.Clamp(transform.position.z, -0.1f, 0.1f));
