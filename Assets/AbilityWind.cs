4366e54e2f7b4118d79afe0776f6a43fe6813697
Fabian <faebu.og@gmail.com>
2022-04-17 06:31:42 +0200

    First ability logic done

--- Assets/AbilityWind.cs
+++ Assets/AbilityWind.cs
@@ -1,20 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class AbilityWind : MonoBehaviour
 {
-    private Rigidbody ballRb;
-    private float speed = 1000f;
+    [SerializeField] private float speed;
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
-            Debug.Log("Ball is in Windzone");
-            ballRb = other.GetComponent<Rigidbody>();
-            ballRb.AddForce(speed * Time.deltaTime,0f,0f);
+            other.gameObject.GetComponent<Rigidbody>().AddForce(-speed*Vector3.right, ForceMode.Impulse);
         }
     }
 }
