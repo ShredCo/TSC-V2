aab22237a3e4e80339cc8e4f776529dbdbde94c4
Fabian <faebu.og@gmail.com>
2022-04-18 22:37:54 +0200

    crash fix

--- Assets/_TSC/_Scripts/Match/Powerpoints/SpawnPowerfields.cs
+++ Assets/_TSC/_Scripts/Match/Powerpoints/SpawnPowerfields.cs
@@ -8,58 +8,57 @@ using Random = UnityEngine.Random;
 
 public class SpawnPowerfields : MonoBehaviour
 {
-    public static SpawnPowerfields instance;
+   public static SpawnPowerfields instance;
 
-    private void Awake()
-    {
-        if (instance == null)
-            instance = this;
-    }
+   private void Awake()
+   {
+       if (instance == null)
+           instance = this;
+   }
 
-    public GameObject powerfieldPrefabRed;
-    public GameObject powerfieldPrefabBlue;
+   public GameObject powerfieldPrefabRed;
+   public GameObject powerfieldPrefabBlue;
 
-    private float xPos;
-    private float zPos;
+   private float xPos;
+   private float zPos;
 
-    public int powerfieldsCountRed;
-    public int powerfieldsCountBlue;
+   public int powerfieldsCountRed;
+   public int powerfieldsCountBlue;
 
-    private void Start()
-    {
-        StartCoroutine(RandomSpawnPowerfieldsRed());
-        StartCoroutine(RandomSpawnPowerfieldsBlue());
+   private void Start()
+   {
+       StartCoroutine(RandomSpawnPowerfieldsRed());
+       StartCoroutine(RandomSpawnPowerfieldsBlue());
 
-    }
+   }
 
-    IEnumerator RandomSpawnPowerfieldsRed()
-    {
-        while (powerfieldsCountRed < 100)
-        {
-            if (powerfieldsCountRed < 3)
-            {
-                yield return new WaitForSeconds(10f);
-                xPos = Random.Range(-0.7f, 0.7f);
-                zPos = Random.Range(-0.35f, 0.35f);
-                Instantiate(powerfieldPrefabRed, new Vector3(xPos, 0.01f, zPos), Quaternion.identity);
-                powerfieldsCountRed += 1;
-            }
-            
-        }
-    }
-    IEnumerator RandomSpawnPowerfieldsBlue()
-    {
-        while (powerfieldsCountBlue < 100)
-        {
-            if (powerfieldsCountBlue < 3)
-            {
-                yield return new WaitForSeconds(10f);
-                xPos = Random.Range(-0.7f, 0.7f);
-                zPos = Random.Range(-0.35f, 0.35f);
-                Instantiate(powerfieldPrefabBlue, new Vector3(xPos, 0.01f, zPos), Quaternion.identity);
-                powerfieldsCountBlue += 1;
-            }
-            
-        }
-    }
+   IEnumerator RandomSpawnPowerfieldsRed()
+   {
+       while (powerfieldsCountRed < 100)
+       {
+           yield return new WaitForSeconds(10f);
+           if (powerfieldsCountRed < 3)
+           {
+               xPos = Random.Range(-0.7f, 0.7f);
+               zPos = Random.Range(-0.35f, 0.35f);
+               Instantiate(powerfieldPrefabRed, new Vector3(xPos, 0.01f, zPos), Quaternion.identity);
+               powerfieldsCountRed += 1;
+           }
+           
+       }
+   }
+   IEnumerator RandomSpawnPowerfieldsBlue()
+   {
+       while (powerfieldsCountBlue < 100)
+       {
+           yield return new WaitForSeconds(10f);
+           if (powerfieldsCountBlue < 3)
+           {
+               xPos = Random.Range(-0.7f, 0.7f);
+               zPos = Random.Range(-0.35f, 0.35f);
+               Instantiate(powerfieldPrefabBlue, new Vector3(xPos, 0.01f, zPos), Quaternion.identity);
+               powerfieldsCountBlue += 1;
+           }
+       }
+   }
 }
