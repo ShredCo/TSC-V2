c57c07d7b9e6832d42506962de8ef5d8d58a871a
Marvin Moreno <marmor0416@gmail.com>
2022-04-24 11:31:07 +0200

    Added button hover sounds

--- Assets/_TSC/_Scripts/Audio/UISoundeffects.cs
+++ Assets/_TSC/_Scripts/Audio/UISoundeffects.cs
@@ -17,33 +17,45 @@ public class UISoundeffects : MonoBehaviour
     public AudioClip ButtonPressMenu;
     public AudioClip ButtonPressCard;
     public AudioClip OpenCardSelectionSound;
+    public AudioClip UINavigation;
 
     public AudioClip SelectionFailed;
     public AudioClip[] CardSelection;
 
     public void CardSelectSound()
     {
+        AudioSource.volume = 1f;
         AudioSource.clip = CardSelection[Random.Range(0, CardSelection.Length)];
         AudioSource.Play();
     }
     public void SelectionFailedSound()
     {
+        AudioSource.volume = 1f;
         AudioSource.clip = SelectionFailed;
         AudioSource.Play();
     }
     public void ButtonClick()
     {
+        AudioSource.volume = 1f;
         AudioSource.clip = ButtonPressMenu;
         AudioSource.Play();
     }
     public void CardClick()
     {
+        AudioSource.volume = 1f;
         AudioSource.clip = ButtonPressCard;
         AudioSource.Play();
     }
     public void OpenCardSelection()
     {
+        AudioSource.volume = 1f;
         AudioSource.clip = OpenCardSelectionSound;
+        AudioSource.Play();
+    }
+    public void UINavigationSound()
+    {
+        AudioSource.volume = 0.5f;
+        AudioSource.clip = UINavigation;
         AudioSource.Play();
     }
 }
