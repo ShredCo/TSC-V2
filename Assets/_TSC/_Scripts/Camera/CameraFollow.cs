a55b8b1cdf89178ee69e06b18bad47c799a787c2
Fabian <faebu.og@gmail.com>
2022-04-28 16:08:31 +0200

    match Camera updates

--- Assets/_TSC/_Scripts/Camera/CameraFollow.cs
+++ Assets/_TSC/_Scripts/Camera/CameraFollow.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform ball;
-    public float lerpSpeed = 0.001f;
+    public Transform Ball;
+    private float cameraFollowSpeed = 5f;
 
     private Vector3 offsetPosition;
     // Update is called once per frame
     void Update()
     {
-        offsetPosition = ball.transform.position;
+        offsetPosition = Ball.transform.position;
         offsetPosition.y = 1.2f;
         offsetPosition.z = 0.7f;
         
-        
-        transform.position = Vector3.Lerp(transform.position, offsetPosition, lerpSpeed * Time.deltaTime); 
+        transform.position = Vector3.Lerp(transform.position, offsetPosition, cameraFollowSpeed * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -0.5f, 0.5f),transform.position.y, transform.position.z);
     }
 }
