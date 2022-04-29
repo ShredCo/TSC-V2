5dada31078a56778d2ba920b0b330dfd93fbe6ca
Fabian <faebu.og@gmail.com>
2022-04-29 14:55:55 +0200

    played around with ball and camera settings again. Should be fine now
    But Ball still has no sound for collision. Something messed up

--- Assets/_TSC/_Scripts/Camera/CameraFollow.cs
+++ Assets/_TSC/_Scripts/Camera/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform Ball;
-    private float cameraFollowSpeed = 5f;
+    private float cameraFollowSpeed = 1.5f;
 
     private Vector3 offsetPosition;
     // Update is called once per frame
