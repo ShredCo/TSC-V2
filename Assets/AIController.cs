982d2e4aa7a7f52330deae6b22fe89e732e85557
Fabian <faebu.og@gmail.com>
2022-04-30 02:23:46 +0200

    setup done

--- Assets/AIController.cs
+++ Assets/AIController.cs
@@ -4,35 +4,34 @@ using UnityEngine;
 
 public class AIController : MonoBehaviour
 {
-    public PoleShooting[] polesShooting;
-    public PolesPlayer[] polesPlayers;
+    
     [SerializeField] private PolesAI[] polesAI;
-    
-    [SerializeField] private float movement = 1f;
-    private Vector2 movementPoleInput;
 
-    public float speed = 1500f;
     
+    private Vector2 movementPoleInput;
+    public float rotationSpeed = 1500f;
+    public float movementSpeed = 2f;
     
     [SerializeField]
-    [Range(0f, 1f)]
+    [Range(-1f, 1f)]
     private float movementInputValue = 0f;
     [SerializeField]
-    [Range(0f, 1f)]
+    [Range(-1f, 1f)]
     private float rotationInputValue = 0f;
     [SerializeField]
     [Range(0, 3)]
-    private int currentPole = 0;
+    private int currentPoleIndexAI = 0;
     
     // Update is called once per frame
     void FixedUpdate()
     {
-        movementPoleInput = new Vector2(rotationInputValue, 0f);
-        movementPoleInput.x *= speed;
+        movementPoleInput = new Vector2(rotationInputValue, movementInputValue);
+        movementPoleInput.x *= rotationSpeed;
+        movementPoleInput.y *= movementSpeed;
         
         Debug.Log("movement input value= " + movementPoleInput.x);
 
         //polesShooting[currentPole].MoveAndRotate(movementPoleInput * Time.deltaTime);
-        polesAI[currentPole].MoveAndRotate(movementPoleInput * Time.deltaTime);
+        polesAI[currentPoleIndexAI].MoveAndRotate(movementPoleInput * Time.deltaTime);
     }
 }
