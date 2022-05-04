d37b1b61007759253d7f1ba8e57212f5bedb7846
Fabian <faebu.og@gmail.com>
2022-05-04 19:03:59 +0200

    Cleanup MoneyObject & ResourceObject Script

--- Assets/_TSC/_Scripts/Items/Items/MoneyObject.cs
+++ Assets/_TSC/_Scripts/Items/Items/MoneyObject.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Random = System.Random;
-
 
 [CreateAssetMenu(fileName = "New money object", menuName = "Inventory System/Items/Money")]
 public class MoneyObject : ItemObject
@@ -12,6 +7,4 @@ public class MoneyObject : ItemObject
     {
         Type = ItemType.Money;
     }
-    
-    // Todo: set a random range between 1/10 & 10/100 & 100/300 ôr something. Depends on how big the moneyItem is.
 }
