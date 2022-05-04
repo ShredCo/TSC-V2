b7f8ad533483b5f8944a38fbff695e749668eb8f
Fabian <faebu.og@gmail.com>
2022-05-04 20:10:05 +0200

    Deleted DisplayInventory Script

--- Assets/_TSC/_Scripts/Items/DefaultObject.cs
+++ Assets/_TSC/_Scripts/Items/DefaultObject.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New default object", menuName = "Inventory System/Items/Default")]
