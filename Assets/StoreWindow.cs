60a6e765f759bd2cabed56f36c30b088a5c40b67
FaebuOG <soly.fabian.g@gmail.com>
2022-06-03 12:45:54 +0200

    git lfs working test?

---
+++ Assets/StoreWindow.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreWindow : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private bool playMovement;
+    private bool moveRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playMovement = true;
+        StartCoroutine(MovementStoreWindowPole());
+
+    }
+
+    private void Update()
+    {
+        if (moveRight)
+        {
+            transform.position += transform.up * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position -= transform.up * moveSpeed * Time.deltaTime;
+        }
+    }
+    IEnumerator MovementStoreWindowPole()
+    {
+        while (playMovement)
+        {
+            Debug.Log("Store Window work");
+            transform.position += transform.right * moveSpeed * Time.deltaTime;
+            yield return new WaitForSeconds(3);
+            transform.position -= transform.right * moveSpeed * Time.deltaTime;
+            yield return new WaitForSeconds(3);
+            moveRight = true;
+            yield return new WaitForSeconds(5);
+            moveRight = false;
+            yield return new WaitForSeconds(5);
+        }
+
+    }
+}
